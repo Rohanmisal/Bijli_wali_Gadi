e03d06a2b687d491f59211e8b1273ef1c30b344a
rohanmisal <99160188+Rohanmisal@users.noreply.github.com>
2024-01-12 17:07:12 +0530

    moving towords

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const App = () =>{
     return (
         <>
-
+        <h1></h1>
         </>
-    )
-}
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
